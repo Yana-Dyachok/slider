4ca5ff984ee3641b91e79776bded059d421ebdec
Yana-Dyachok <annetart1994@gmail.com>
2023-08-10 18:51:56 +0300

    feat: add code for press button

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const prevButton=document.querySelector('.prev-btn'),
 nextButton=document.querySelector('.next-btn'),
 sliderLine=document.querySelector('.slider-line');
-let offset=0;
+let offset=0, n=7, width=43.75;
 nextButton.addEventListener('click',()=>{
-    offset+=43.75;
-    sliderLine.style.left=offter+'rem';
+    (offset<n*width)?offset+=width:offset=n*width;
+    sliderLine.style.left=-offset+'rem';
 })
+
+prevButton.addEventListener('click',()=>{
+    (offset>0)?offset-=width:offset=0;
+    sliderLine.style.left=-offset+'rem';
+})
+
