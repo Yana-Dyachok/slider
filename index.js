e8df622103703d2222c49c4972aba7e134b5126a
Yana-Dyachok <annetart1994@gmail.com>
2023-08-12 15:21:48 +0300

    feat: add styles for body

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const prevButton=document.querySelector('.prev-btn'),
 nextButton=document.querySelector('.next-btn'),
 sliderLine=document.querySelector('.slider-line');
 let offset=0, n=7, width=62.5;
-nextButton.addEventListener('click',()=>{
-    (offset<(n-1)*width)?offset+=width:offset=(n-1)*width;
-    sliderLine.style.left=-offset+'rem';
-})
 
-prevButton.addEventListener('click',()=>{
-    (offset>0)?offset-=width:offset=0;
-    sliderLine.style.left=-offset+'rem';
-})
+function clickButtons() {
+    nextButton.addEventListener('click',()=>{
+        (offset<(n-1)*width)?offset+=width:offset=(n-1)*width;
+        sliderLine.style.left=-offset+'rem';
+    //nextButton.disabled=(offset===(n-1)*width&&prevButton.disabled===false)?true:false;
+    })
+   
+    prevButton.addEventListener('click',()=>{
+        (offset>0)?offset-=width:offset=0;
+        sliderLine.style.left=-offset+'rem';
+       // prevButton.disabled=(offset===0)?true:false;
+    })
+}
+clickButtons()
