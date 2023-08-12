663d646563c473e5c46853ee27839d98d6362a6c
Yana-Dyachok <annetart1994@gmail.com>
2023-08-12 16:22:01 +0300

    feat: add function wich make disable btn and change code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const prevButton=document.querySelector('.prev-btn'),
-nextButton=document.querySelector('.next-btn'),
-sliderLine=document.querySelector('.slider-line');
-let offset=0, n=7, width=62.5;
+const prevButton = document.querySelector('.prev-btn'),
+      nextButton = document.querySelector('.next-btn'),
+      sliderLine = document.querySelector('.slider-line');
+const n = 7,  width = 62.5;
+let offset = 0;
 
-function clickButtons() {
-    nextButton.addEventListener('click',()=>{
-        (offset<(n-1)*width)?offset+=width:offset=(n-1)*width;
-        sliderLine.style.left=-offset+'rem';
-    //nextButton.disabled=(offset===(n-1)*width&&prevButton.disabled===false)?true:false;
-    })
-   
-    prevButton.addEventListener('click',()=>{
-        (offset>0)?offset-=width:offset=0;
-        sliderLine.style.left=-offset+'rem';
-       // prevButton.disabled=(offset===0)?true:false;
-    })
+function updateButtonState() {
+    nextButton.disabled = offset >= (n - 1) * width;
+    prevButton.disabled = offset === 0;
 }
-clickButtons()
+
+function clickButton(isNext) {
+    offset = isNext ? Math.min(offset + width, (n - 1) * width) : Math.max(offset - width, 0);
+    sliderLine.style.left = -offset + 'rem';
+    updateButtonState();
+}
+
+nextButton.addEventListener('click', () => clickButton(true));
+prevButton.addEventListener('click', () => clickButton(false));
