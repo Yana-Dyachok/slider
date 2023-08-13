d929fd66ea3c3c8b498bc02e24d189d93d547060
Yana-Dyachok <annetart1994@gmail.com>
2023-08-13 21:21:58 +0300

    feat: get  width from slider

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const prevButton = document.querySelector('.prev-btn'),
-      nextButton = document.querySelector('.next-btn'),
-      sliderLine = document.querySelector('.slider-line');
-const n = 7,  width = 62.5;
-let offset = 0;
+    nextButton = document.querySelector('.next-btn'),
+    sliderLine = document.querySelector('.slider-line'),
+    sliderElement = document.querySelector('.slider'),
+    sliderStyles = window.getComputedStyle(sliderElement),
+    widthInPixels = parseFloat(sliderStyles.getPropertyValue('width'));
+
+const baseFontSizeInPixels = 16,
+    n = 7;
+let offset = 0,
+    width = widthInPixels / baseFontSizeInPixels;
 
 function updateButtonState() {
     nextButton.disabled = offset >= (n - 1) * width;
@@ -10,7 +16,9 @@ function updateButtonState() {
 }
 
 function clickButton(isNext) {
-    offset = isNext ? Math.min(offset + width, (n - 1) * width) : Math.max(offset - width, 0);
+    offset = isNext
+        ? Math.min(offset + width, (n - 1) * width)
+        : Math.max(offset - width, 0);
     sliderLine.style.left = -offset + 'rem';
     updateButtonState();
 }
